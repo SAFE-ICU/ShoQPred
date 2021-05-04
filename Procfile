f128f6ce497d568204932e62732d2ceb13e895ff
Harsh <raptor419heavy@gmail.com>
2021-05-04 14:28:30 +0530

    Upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn core.wsgi --log-file=- 
+web: gunicorn --config gunicorn-heroku.py core.wsgi
---
+++ gunicorn-heroku.py
@@ -0,0 +1,7 @@
+bind = '0.0.0.0:8000'
+workers = 1
+timeout = 120
+accesslog = '-'
+loglevel = 'debug'
+capture_output = True
+enable_stdio_inheritance = True
