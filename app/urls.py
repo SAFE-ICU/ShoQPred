9e8edc63c2c1ef35b49bbab87ab273633ee57f03
Harsh <raptor419heavy@gmail.com>
2021-05-04 10:03:03 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('authors', views.authors, name='authors'),
+    path('demo', views.authors, name='demo'),
+    path('git', views.git, name='git'),
+    path('start', views.start, name='start'),
+    path('uploadcsv', views.uploadcsv, name='uploadcsv'),
+    path('preprocess', views.preprocessing, name='preprocessing'),
+    path('showPreprocess', views.showPreprocess, name='showpreprocess'),
+    path('classify', views.classify, name='classify'),
+    path('download/<int:idx>/', views.download, name='download'),
+    re_path(r'^.*\.html', views.pages, name='pages'),
+]
